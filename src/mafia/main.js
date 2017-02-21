0dcfa422cfcee7b650ff89d165477b3e48a3d69c
marc <cece.marc.vu@gmail.com>
2017-02-21 09:20:43 +0100

    refacto: less brackets

--- src/mafia/main.js
+++ src/mafia/main.js
@@ -75,71 +75,70 @@ let main = {
         .id
 
       bot.on('message', data => {
-        if (data.type == 'message') {
-          if (data.text) {
-            if (data.text.slice(0, 6) == '!mafia') {
+        if (data.type == 'message' && data.text) {
+          if (data.text.slice(0, 6) == '!mafia') {
 
-              const command = _.lowerCase(data.text.slice(7))
+            const command = _.lowerCase(data.text.slice(7))
 
-              switch (command) {
+            switch (command) {
 
-                case 'init':
-                  initializer(botID)
-                  break;
+              case 'init':
+                initializer(botID)
+                break;
 
-                case 'newgame':
-                  bot.getGroups()
-                    .then(dataGroups => bot.getChannels()
-                      .then(dataChannels => {
-                        var chans = []
-                        _.forEach(dataGroups.groups, group => {
-                          if (_.indexOf(_.keys(GROUPS), group.name) > -1) {
-                            chans.push({ name: group.name, id: group.id, team: GROUPS[group.name] })
-                          }
-                        })
-                        _.forEach(dataChannels.channels, channel => {
-                          if (_.indexOf(_.keys(CHANNELS), channel.name) > -1) {
-                            chans.push({ name: channel.name, id: channel.id, team: CHANNELS[channel.name] })
-                          }
-                        })
-                        bot.getChannel(_.find(chans, o => o.team == 'town-room')
-                            .name)
-                          .then(data => {
-                            bot.getUsers()
-                              .then(dataUsers => {
-                                var players = []
-                                _.forEach(dataUsers.members, member => {
-                                  if (!member.is_bot && member.name != 'slackbot' && member.name != process.env.MAFIA_API_NAME) {
-                                    if (_.indexOf(data.members, member.id) > -1)
-                                      players.push({ id: member.id, name: member.name })
-                                  }
-                                })
-                                if (!gameInitiated) {
-                                  game = new Game(gameEmitter, webApi, players, chans)
-                                  game.init()
-                                  gameInitiated = true
+              case 'newgame':
+                bot.getGroups()
+                  .then(dataGroups => bot.getChannels()
+                    .then(dataChannels => {
+                      var chans = []
+                      _.forEach(dataGroups.groups, group => {
+                        if (_.indexOf(_.keys(GROUPS), group.name) > -1) {
+                          chans.push({ name: group.name, id: group.id, team: GROUPS[group.name] })
+                        }
+                      })
+                      _.forEach(dataChannels.channels, channel => {
+                        if (_.indexOf(_.keys(CHANNELS), channel.name) > -1) {
+                          chans.push({ name: channel.name, id: channel.id, team: CHANNELS[channel.name] })
+                        }
+                      })
+                      bot.getChannel(_.find(chans, o => o.team == 'town-room')
+                          .name)
+                        .then(data => {
+                          bot.getUsers()
+                            .then(dataUsers => {
+                              var players = []
+                              _.forEach(dataUsers.members, member => {
+                                if (!member.is_bot && member.name != 'slackbot' && member.name != process.env.MAFIA_API_NAME) {
+                                  if (_.indexOf(data.members, member.id) > -1)
+                                    players.push({ id: member.id, name: member.name })
                                 }
                               })
-                          })
-                      }))
-                  break
+                              if (!gameInitiated) {
+                                game = new Game(gameEmitter, webApi, players, chans)
+                                game.init()
+                                gameInitiated = true
+                              }
+                            })
+                        })
+                    }))
+                break
 
-                case 'start':
-                  if (gameInitiated && !gamehasStarted) {
-                    game.start()
-                    gamehasStarted = true
-                  }
-                  break
+              case 'start':
+                if (gameInitiated && !gamehasStarted) {
+                  game.start()
+                  gamehasStarted = true
+                }
+                break
 
-                case 'force-restart ' + process.env.MAFIA_PASSWORD:
-                  break
+              case 'force-restart ' + process.env.MAFIA_PASSWORD:
+                break
 
-                default:
-                  break
+              default:
+                break
 
-              }
             }
           }
+
         }
         if (data.type == 'message' && data.user != botID) {
           gameEmitter.emit('newMessage', data)
